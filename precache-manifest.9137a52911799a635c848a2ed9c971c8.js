fa6f19be63f825fe8114cd23b8dc0f393e087a78
handrawan112 <handrawanwawan012@gmail.com>
2020-07-05 15:59:16 +0700

    Updates

---
+++ precache-manifest.9137a52911799a635c848a2ed9c971c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4943112a0238e867a5505d0930013d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cf04a12506cd43def0f",
+    "url": "/static/css/main.e37cbce5.chunk.css"
+  },
+  {
+    "revision": "168d5ca65e5ac00c0427",
+    "url": "/static/js/2.6e4b3d61.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6e4b3d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf04a12506cd43def0f",
+    "url": "/static/js/main.37a23106.chunk.js"
+  },
+  {
+    "revision": "5741d1e21652314c9561",
+    "url": "/static/js/runtime-main.e5b64ef3.js"
+  }
+]);
